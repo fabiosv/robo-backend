14d6509f3c645d72ad741351f4fb3a37962d95db
Valonga, Fabio <fabiosv.etep@gmail.com>
2019-05-12 02:39:53 -0300

    create models and robo service

---
+++ RoboAPI/Models/Head.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RoboAPI.Models
+{
+    public class Head
+    {
+        private int rotation = 3;
+        private int inclination = 2;
+        public Head()
+        {
+
+        }
+ 
+        public int getRotation()
+        {
+            return this.rotation;
+        }
+
+        public void setRotation(int rotation)
+        {
+            if(this.isMovementValid(rotation, this.rotation) || this.inclination == 3)
+            {
+                // TODO: raise an error
+            } else
+            {
+                this.rotation = rotation;
+            }
+        }
+
+        public int getInclination()
+        {
+            return this.inclination;
+        }
+
+        public void setInclination(int inclination)
+        {
+            if (this.isMovementValid(inclination, this.inclination))
+            {
+                // TODO: raise an error
+            } else
+            {
+                this.inclination = inclination;
+            }
+        }
+
+        private bool isMovementValid(int var, int attribute)
+        {
+            return var - attribute > 1 || attribute - var > 1;
+        }
+    }
+}
---
+++ RoboAPI/Services/ROBO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using RoboAPI.Models;
+
+namespace RoboAPI.Services
+{
+    public class ROBO
+    {
+        Arms leftArm = new Arms();
+        Arms rightArm = new Arms();
+        Head head = new Head();
+
+        public void main()
+        {
+            // head.setRotation(4);
+        }
+        
+    }
+}
