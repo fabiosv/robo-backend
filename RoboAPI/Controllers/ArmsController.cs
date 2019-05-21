02434a0a4aa4da71766d2591157bd66e26b76757
Valonga, Fabio <fabiosv.etep@gmail.com>
2019-05-20 22:07:34 -0300

    json formatter + head api

---
+++ RoboAPI/Controllers/ArmsController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using RoboAPI.Services;
+
+namespace RoboAPI.Controllers
+{
+    public enum Robo { Head, LeftArm, RightArm }
+    public class ArmsController : ApiController
+    {
+        private ROBOService robo_service = ROBOService.Instance;
+
+        // GET: api/Arms
+        public Dictionary<string,Dictionary<string,int>> Get()
+        {
+            Dictionary<string, Dictionary<string, int>> robo = new Dictionary<string, Dictionary<string, int>>();
+            robo.Add("LeftArm", robo_service.DictLeftArm());
+            robo.Add("RightArm", robo_service.DictRightArm());
+            return robo;
+        }
+
+        // PUT: api/Arms/left|right
+        public IHttpActionResult Put([FromBody]Models.Arms value)
+        {
+            try
+            {
+                if (value.Side == "left")
+                {
+                    this.robo_service.LeftArm.Elbow = value.Elbow;
+                    this.robo_service.LeftArm.Wrist = value.Wrist;
+                    return Ok();
+                }
+
+                if (value.Side == "right")
+                {
+                    this.robo_service.RightArm.Elbow = value.Elbow;
+                    this.robo_service.RightArm.Wrist = value.Wrist;
+                    return Ok();
+                }
+                return NotFound();
+            } catch(Exception e)
+            {
+                return InternalServerError(e);
+            }
+            
+        }
+    }
+}
---
+++ RoboAPI/Controllers/HeadController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using RoboAPI.Services;
+
+namespace RoboAPI.Controllers
+{
+    public class HeadController : ApiController
+    {
+        private ROBOService robo_service = ROBOService.Instance;
+
+        // GET: api/Head
+        public Dictionary<string, Dictionary<string, int>> Get()
+        {
+            Dictionary<string, Dictionary<string, int>> robo = new Dictionary<string, Dictionary<string, int>>();
+            robo.Add("Head", robo_service.DictHead());
+            return robo;
+        }
+
+        // PUT: api/Head/
+        public IHttpActionResult Put([FromBody]Models.Head value)
+        {
+            try
+            {
+                this.robo_service.HeadRotation = value.Rotation;
+                this.robo_service.HeadInclination = value.Inclination;
+                return Ok();
+            }
+            catch(Exception e)
+            {
+                System.Console.WriteLine("Error: " + e.ToString());
+                return InternalServerError(e);
+            }
+        }
+    }
+}
